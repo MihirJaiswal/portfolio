c17255e8404ca733c249ed1d997961325755d41a
MihirJaiswal <jaiswalmihir.business@gmail.com>
2025-09-14 12:41:19 +0530

    some chnges

--- src/components/Cards/Service.tsx
+++ src/components/Cards/Service.tsx
@@ -60,7 +60,7 @@ export default function Service() {
                     <div className="w-12 h-12 rounded-full bg-white border border-neutral-300 dark:bg-neutral-950 dark:border-neutral-700 hidden xs:flex items-center justify-center mr-4 group-hover:bg-black group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black transition-all">
                       <span className="text-sm font-semibold">{service.id}</span>
                     </div>
-                    <div className="flex-1 py-2 px-4 rounded-lg xs:rounded-4xl bg-white dark:bg-neutral-950 text-black dark:text-white border group-hover:bg-black group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black transition-all">
+                    <div className="flex-1 py-2 px-4 rounded-lg xs:rounded-4xl bg-white dark:bg-neutral-950 text-black dark:text-white border group-hover:bg-black group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black transition-all mx-4 xs:mx-0">
                       <p className="text-center font-semibold text-neutral-800 dark:text-neutral-200 group-hover:text-white dark:group-hover:text-black">{service.title}</p>
                     </div>
                     <div className="w-12 h-12 rounded-full bg-white border border-neutral-300 dark:bg-neutral-950 dark:border-neutral-700 hidden xs:flex items-center justify-center ml-4 group-hover:bg-black group-hover:text-white dark:group-hover:bg-white dark:group-hover:text-black transition-all">
